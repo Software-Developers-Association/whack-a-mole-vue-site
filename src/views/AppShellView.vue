defe196ce67962541810aceddf3e7f4f1ebde538
Corbin Crutchley <crutchcorn@gmail.com>
2020-02-27 16:39:54 -0800

    Added vuetify

--- src/views/AppShellView.vue
+++ src/views/AppShellView.vue
@@ -1,37 +1,63 @@
 <template>
-  <div id="app">
-    <p>Should work, but doesn't</p>
-    <router-view></router-view>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/game">
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Game</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue'
-import UnityDisplay from '../components/UnityDisplay.vue'
+import HelloWorld from "../components/HelloWorld.vue";
+import UnityDisplay from "../components/UnityDisplay.vue";
 
 export default {
-  name: 'AppShellView'
-}
+  name: "AppShellView",
+  data: function() {
+    return {
+      drawer: false
+    }
+  }
+};
 
 export const routes = [
   {
-    path: '',
+    path: "",
     component: HelloWorld
   },
   {
-    path: 'game',
+    path: "game",
     component: UnityDisplay
   }
-]
+];
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
