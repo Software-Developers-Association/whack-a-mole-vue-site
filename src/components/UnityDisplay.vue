aa5a70758a8202cefe2031c88c8fb6ddc4a47bfd
Corbin Crutchley <crutchcorn@gmail.com>
2020-02-27 16:15:53 -0800

    Got initial game working

---
+++ src/components/UnityDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <unity src="Build/WebGL.json" width="1000" height="600" unityLoader="Build/UnityLoader.js"></unity>  
+  </div>
+</template>
+
+<script>
+import Unity from 'vue-unity-webgl'
+
+export default {
+  name: 'UnityDisplay',
+  components: {
+    Unity
+  }
+}
+</script>
+
+<style scoped>
+</style>
